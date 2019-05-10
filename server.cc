3c6bb81f18505836848e657cd70d59e83d4e844e
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-05-10 12:41:41 +0300

    upd

--- server.cc
+++ server.cc
@@ -25,8 +25,6 @@ using session::SessionService;
 
 void TestConnectTarantool();
 
-Storage storage("127.0.0.1:3302");
-
 class ServiceSessionServer final : public SessionService::Service {
 public:
     Status Save(ServerContext* context, const SaveRequest* request, SaveResponse* response) override {
@@ -35,13 +33,13 @@ public:
     }
 
     Status Get(ServerContext* context, const GetRequest* request, GetResponse* response) override {
-        // implement me
         std::cout << "receive message" << std::endl;
         std::cout << "sessid: " << request->sessid() << std::endl;
         response->set_userid(777);
         response->set_access_token("some-access-token");
         response->set_refresh_token("some-refrech-token");
 
+        Storage storage("127.0.0.1:3302");
         storage.getBySessId(request->sessid());
 
         return Status::OK;
