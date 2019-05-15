8b6583b16274954eeb4ed4ba5c62724a412124e8
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-05-15 08:32:07 +0300

    add help parse command

--- config.cc
+++ config.cc
@@ -1,6 +1,29 @@
+#include <iostream>
+#include <string>
+
 class Config
 {
 public:
     Config(int argc, char** argv) {
+        // initialize defaults
+        this->is_help = false;
+
+        if (argc == 1) {
+            // run without params
+            return;
+        }
+
+        for (int i = 1; i < argc; i++) {
+            std::string paramName(argv[i]);
+
+            if (paramName.compare("-h") == 0 || paramName.compare("--help") == 0) {
+                this->is_help = true;
+                continue;
+            }
+
+            std::cout << "param: " << argv[i] << std::endl;
+        }
     }
+private:
+    bool is_help;
 };
